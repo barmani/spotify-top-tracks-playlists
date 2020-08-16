96b7b1cdbadba0de6cf5660d8e7b3bb887ef8d34
Brendan Armani <brendanarmani@gmail.com>
2020-08-15 23:06:29 -0400

    Backend now has sign in functionality and endpoint to return top short, medium, and long term tracks

---
+++ backend/app.py
@@ -0,0 +1,95 @@
+import os
+from flask import Flask, session, request, redirect
+from flask_session import Session
+import spotipy
+import uuid
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
+
+@app.route('/')
+def index():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private user-top-read',
+                                                cache_path=session_cache_path(),
+                                                show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.get_cached_token():
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 4. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<a href="/top_tracks">Top Tracks</a>' \
+
+
+@app.route('/sign_out')
+def sign_out():
+    os.remove(session_cache_path())
+    session.clear()
+    try:
+        # Remove the CACHE file (.cache-test) so that a new user can authorize.
+        os.remove(session_cache_path())
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+    return redirect('/')
+
+@app.route('/top_tracks')
+def get_top_tracks():
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+    top_short_term = spotify.current_user_top_tracks(time_range='short_term')
+    top_med_term = spotify.current_user_top_tracks(time_range='medium_term')
+    top_long_term = spotify.current_user_top_tracks(time_range='long_term')
+    return {
+        'short_term': extract_top_track_information(top_short_term),
+        'med_term': extract_top_track_information(top_med_term),
+        'long_term': extract_top_track_information(top_long_term)
+    }
+
+def extract_top_track_information(tracks_data):
+    extracted_data = []
+    for track_data in tracks_data['items']:
+        extracted_data.append(
+            {
+                'artists': [artist_data['name'] for artist_data in track_data['artists']],
+                'song_name': track_data['name']
+            }
+        )
+    return extracted_data
+
+
+@app.route('/current_user')
+def current_user():
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
+    if not auth_manager.get_cached_token():
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user()
+
+
+if __name__ == '__main__':
+	app.run(threaded=True, port=int(os.environ.get("PORT", 5000)))
